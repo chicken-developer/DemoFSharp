336c6fe6a8e0efc66a2ada1717c60de8e0b24480
chicken-developer <quynh.fullstackdev@gmail.com>
2021-01-01 22:09:21 +0700

    [Quynh] Refactor project structure

--- FSharpSources/obj/Debug/net5.0/Unity3dClassLibrary.AssemblyInfo.fs
+++ FSharpSources/obj/Debug/net5.0/Unity3dClassLibrary.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("Unity3dClassLibrary")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("FShrapLibrary")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("Unity3dClassLibrary")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("Unity3dClassLibrary")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("FShrapLibrary")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("FShrapLibrary")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
